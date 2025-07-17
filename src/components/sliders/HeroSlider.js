73ecc2eba929ee30636b49ec892d208fb1c6577f
GK <gabykassab0@gmail.com>
2025-07-18 00:26:58 +0300

    Updated Black XS image

--- src/components/sliders/HeroSlider.js
+++ src/components/sliders/HeroSlider.js
@@ -124,6 +124,65 @@
 
 
 
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import { Pagination, A11y, Autoplay } from 'swiper';
+// import productsData from '../../data/productsData';
+
+// import 'swiper/scss';
+// import 'swiper/scss/autoplay';
+// import 'swiper/scss/pagination';
+
+// const HeroSlider = () => {
+//   const heroProducts = productsData.filter(item => item.tag === 'hero-product');
+
+//   return (
+//     <Swiper
+//       modules={[Pagination, A11y, Autoplay]}
+//       loop={true}
+//       speed={400}
+//       spaceBetween={100}
+//       slidesPerView={1}
+//       pagination={{ clickable: true }}
+//       autoplay={{
+//         delay: 2000,
+//         disableOnInteraction: false,
+//       }}
+//     >
+//       {heroProducts.map((item, i) => {
+//         const { id, title, tagline, heroImage, path } = item;
+
+//         return (
+//           <SwiperSlide
+//             key={id}
+//             className={`wrapper hero_wrapper hero_slide-${i}`}
+//           >
+//             <div className="hero_item_txt">
+//               <h3>{title}</h3>
+//               {/* <h1>{tagline}</h1> */}
+//               <h1 className="tagline">{tagline}</h1>
+
+//               {/* ✅ Go directly to the path */}
+//               <Link to={path} className="btn">
+//                 Shop Now
+//               </Link>
+//             </div>
+
+//             <figure className="hero_item_img">
+//               <img src={heroImage} alt={`Slide for ${title}`} />
+//             </figure>
+//           </SwiperSlide>
+//         );
+//       })}
+//     </Swiper>
+//   );
+// };
+
+// export default HeroSlider;
+
+
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -139,6 +198,7 @@ const HeroSlider = () => {
 
   return (
     <Swiper
+      id="hero-swiper" // ✅ for targeting in CSS
       modules={[Pagination, A11y, Autoplay]}
       loop={true}
       speed={400}
@@ -160,11 +220,9 @@ const HeroSlider = () => {
           >
             <div className="hero_item_txt">
               <h3>{title}</h3>
-              {/* <h1>{tagline}</h1> */}
               <h1 className="tagline">{tagline}</h1>
 
-              {/* ✅ Go directly to the path */}
-              <Link to={path} className="btn">
+              <Link to={path} className="btn shop-btn">
                 Shop Now
               </Link>
             </div>
