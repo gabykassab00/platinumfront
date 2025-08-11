b9ecb20ffc6b9f29701aff15f328606ec12840bd
GK <gabykassab0@gmail.com>
2025-08-12 00:50:35 +0300

    Updated Black XS image

--- src/data/footerData.js
+++ src/data/footerData.js
@@ -1,4 +1,136 @@
-import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn } from 'react-icons/fa';
+// import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn } from 'react-icons/fa';
+
+// export const footMenu = [
+//     {
+//         id: 1,
+//         title: "Help",
+//         menu: [
+//             {
+//                 id: 1,
+//                 link: "FAQs",
+//                 path: "/"
+//             },
+//             {
+//                 id: 2,
+//                 link: "Track Order",
+//                 path: "/"
+//             },
+//             {
+//                 id: 3,
+//                 link: "Cancel Order",
+//                 path: "/"
+//             },
+//             {
+//                 id: 4,
+//                 link: "Return Order",
+//                 path: "/"
+//             },
+//             {
+//                 id: 5,
+//                 link: "Warranty Info",
+//                 path: "/"
+//             },
+//         ]
+//     },
+//     {
+//         id: 2,
+//         title: "Policies",
+//         menu: [
+//             {
+//                 id: 1,
+//                 link: "Return Policy",
+//                 path: "/"
+//             },
+//             {
+//                 id: 2,
+//                 link: "Security",
+//                 path: "/"
+//             },
+//             {
+//                 id: 3,
+//                 link: "Sitemap",
+//                 path: "/"
+//             },
+//             {
+//                 id: 4,
+//                 link: "Privacy Policy",
+//                 path: "/"
+//             },
+//             {
+//                 id: 5,
+//                 link: "Terms & Conditions",
+//                 path: "/"
+//             },
+//         ]
+//     },
+//     {
+//         id: 3,
+//         title: "Company",
+//         menu: [
+//             {
+//                 id: 1,
+//                 link: "About Us",
+//                 path: "/"
+//             },
+//             {
+//                 id: 2,
+//                 link: "Contact Us",
+//                 path: "/"
+//             },
+//             {
+//                 id: 3,
+//                 link: "Service Centres",
+//                 path: "/"
+//             },
+//             {
+//                 id: 4,
+//                 link: "Careers",
+//                 path: "/"
+//             },
+//             {
+//                 id: 5,
+//                 link: "Affiliates",
+//                 path: "/"
+//             },
+//         ]
+//     }
+// ];
+
+// export const footSocial = [
+//     {
+//         id: 1,
+//         icon: <FaFacebookF />,
+//         path: "/",
+//     },
+//     {
+//         id: 2,
+//         icon: <FaTwitter />,
+//         path: "/",
+//     },
+//     {
+//         id: 3,
+//         icon: <FaInstagram />,
+//         path: "/",
+//     },
+//     {
+//         id: 4,
+//         icon: <FaLinkedinIn />,
+//         path: "/",
+//     },
+// ];
+
+
+
+
+
+
+
+
+
+
+
+
+import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn, FaTiktok, FaYoutube, FaSnapchat } from 'react-icons/fa';
 
 export const footMenu = [
     {
@@ -8,27 +140,27 @@ export const footMenu = [
             {
                 id: 1,
                 link: "FAQs",
-                path: "/"
+                path: "/faqs"
             },
             {
                 id: 2,
                 link: "Track Order",
-                path: "/"
+                path: "/track-order"
             },
             {
                 id: 3,
                 link: "Cancel Order",
-                path: "/"
+                path: "/cancel-order"
             },
             {
                 id: 4,
                 link: "Return Order",
-                path: "/"
+                path: "/return-policy"
             },
             {
                 id: 5,
                 link: "Warranty Info",
-                path: "/"
+                path: "/warranty"
             },
         ]
     },
@@ -39,27 +171,27 @@ export const footMenu = [
             {
                 id: 1,
                 link: "Return Policy",
-                path: "/"
+                path: "/return-policy"
             },
             {
                 id: 2,
                 link: "Security",
-                path: "/"
+                path: "/security"
             },
             {
                 id: 3,
                 link: "Sitemap",
-                path: "/"
+                path: "/sitemap"
             },
             {
                 id: 4,
                 link: "Privacy Policy",
-                path: "/"
+                path: "/privacy"
             },
             {
                 id: 5,
                 link: "Terms & Conditions",
-                path: "/"
+                path: "/terms"
             },
         ]
     },
@@ -70,27 +202,27 @@ export const footMenu = [
             {
                 id: 1,
                 link: "About Us",
-                path: "/"
+                path: "/about"
             },
             {
                 id: 2,
                 link: "Contact Us",
-                path: "/"
+                path: "/contact"
             },
             {
                 id: 3,
                 link: "Service Centres",
-                path: "/"
+                path: "/service-centers"
             },
             {
                 id: 4,
                 link: "Careers",
-                path: "/"
+                path: "/careers"
             },
             {
                 id: 5,
                 link: "Affiliates",
-                path: "/"
+                path: "/affiliates"
             },
         ]
     }
@@ -100,21 +232,26 @@ export const footSocial = [
     {
         id: 1,
         icon: <FaFacebookF />,
-        path: "/",
+        path: "https://www.facebook.com/share/16mFGNDoB2/?mibextid=wwXIfr",
     },
     {
         id: 2,
-        icon: <FaTwitter />,
-        path: "/",
+        icon: <FaInstagram />,
+        path: "https://www.instagram.com/platinumperfumes.lb?igsh=MWpvNGhqODE3Z2hwMg==",
     },
     {
         id: 3,
-        icon: <FaInstagram />,
-        path: "/",
+        icon: <FaTiktok />,
+        path: "https://www.tiktok.com/@platinumperfumes.lb1?_t=ZS-8yl8PGeWCCm&_r=1",
     },
     {
         id: 4,
-        icon: <FaLinkedinIn />,
-        path: "/",
+        icon: <FaYoutube />,
+        path: "https://youtube.com/@platinumperfumes?si=aWdt_ZGY54Yxb2ye",
     },
+    {
+        id: 5,
+        icon: <FaSnapchat />,
+        path: "https://t.snapchat.com/WVMktY8Q",
+    }
 ];
