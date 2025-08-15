46da44962e861d5ba048cdc9314e8dee298b1bc1
GK <gabykassab0@gmail.com>
2025-08-15 12:40:21 +0300

    Updated Black XS image

--- src/data/footerData.js
+++ src/data/footerData.js
@@ -130,128 +130,229 @@
 
 
 
-import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn, FaTiktok, FaYoutube, FaSnapchat } from 'react-icons/fa';
+// import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn, FaTiktok, FaYoutube, FaSnapchat } from 'react-icons/fa';
+
+// export const footMenu = [
+//     {
+//         id: 1,
+//         title: "Help",
+//         menu: [
+//             {
+//                 id: 1,
+//                 link: "FAQs",
+//                 path: "/faqs"
+//             },
+//             {
+//                 id: 2,
+//                 link: "Track Order",
+//                 path: "/track-order"
+//             },
+//             {
+//                 id: 3,
+//                 link: "Cancel Order",
+//                 path: "/cancel-order"
+//             },
+//             {
+//                 id: 4,
+//                 link: "Return Order",
+//                 path: "/return-policy"
+//             },
+//             {
+//                 id: 5,
+//                 link: "Warranty Info",
+//                 path: "/warranty"
+//             },
+//         ]
+//     },
+//     {
+//         id: 2,
+//         title: "Policies",
+//         menu: [
+//             {
+//                 id: 1,
+//                 link: "Return Policy",
+//                 path: "/return-policy"
+//             },
+//             {
+//                 id: 2,
+//                 link: "Security",
+//                 path: "/security"
+//             },
+//             {
+//                 id: 3,
+//                 link: "Sitemap",
+//                 path: "/sitemap"
+//             },
+//             {
+//                 id: 4,
+//                 link: "Privacy Policy",
+//                 path: "/privacy"
+//             },
+//             {
+//                 id: 5,
+//                 link: "Terms & Conditions",
+//                 path: "/terms"
+//             },
+//         ]
+//     },
+//     {
+//         id: 3,
+//         title: "Company",
+//         menu: [
+//             {
+//                 id: 1,
+//                 link: "About Us",
+//                 path: "/about"
+//             },
+//             {
+//                 id: 2,
+//                 link: "Contact Us",
+//                 path: "/contact"
+//             },
+//             {
+//                 id: 3,
+//                 link: "Service Centres",
+//                 path: "/service-centers"
+//             },
+//             {
+//                 id: 4,
+//                 link: "Careers",
+//                 path: "/careers"
+//             },
+//             {
+//                 id: 5,
+//                 link: "Affiliates",
+//                 path: "/affiliates"
+//             },
+//         ]
+//     }
+// ];
+
+// export const footSocial = [
+//     {
+//         id: 1,
+//         icon: <FaFacebookF />,
+//         path: "https://www.facebook.com/share/16mFGNDoB2/?mibextid=wwXIfr",
+//     },
+//     {
+//         id: 2,
+//         icon: <FaInstagram />,
+//         path: "https://www.instagram.com/platinumperfumes.lb?igsh=MWpvNGhqODE3Z2hwMg==",
+//     },
+//     {
+//         id: 3,
+//         icon: <FaTiktok />,
+//         path: "https://www.tiktok.com/@platinumperfumes.lb1?_t=ZS-8yl8PGeWCCm&_r=1",
+//     },
+//     {
+//         id: 4,
+//         icon: <FaYoutube />,
+//         path: "https://youtube.com/@platinumperfumes?si=aWdt_ZGY54Yxb2ye",
+//     },
+//     {
+//         id: 5,
+//         icon: <FaSnapchat />,
+//         path: "https://t.snapchat.com/WVMktY8Q",
+//     }
+// ];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import { 
+  FaFacebookF, 
+  FaTwitter, 
+  FaInstagram, 
+  FaLinkedinIn, 
+  FaTiktok, 
+  FaYoutube, 
+  FaSnapchat,
+  FaMapMarkerAlt // added for location
+} from 'react-icons/fa';
 
 export const footMenu = [
-    {
-        id: 1,
-        title: "Help",
-        menu: [
-            {
-                id: 1,
-                link: "FAQs",
-                path: "/faqs"
-            },
-            {
-                id: 2,
-                link: "Track Order",
-                path: "/track-order"
-            },
-            {
-                id: 3,
-                link: "Cancel Order",
-                path: "/cancel-order"
-            },
-            {
-                id: 4,
-                link: "Return Order",
-                path: "/return-policy"
-            },
-            {
-                id: 5,
-                link: "Warranty Info",
-                path: "/warranty"
-            },
-        ]
-    },
-    {
-        id: 2,
-        title: "Policies",
-        menu: [
-            {
-                id: 1,
-                link: "Return Policy",
-                path: "/return-policy"
-            },
-            {
-                id: 2,
-                link: "Security",
-                path: "/security"
-            },
-            {
-                id: 3,
-                link: "Sitemap",
-                path: "/sitemap"
-            },
-            {
-                id: 4,
-                link: "Privacy Policy",
-                path: "/privacy"
-            },
-            {
-                id: 5,
-                link: "Terms & Conditions",
-                path: "/terms"
-            },
-        ]
-    },
-    {
-        id: 3,
-        title: "Company",
-        menu: [
-            {
-                id: 1,
-                link: "About Us",
-                path: "/about"
-            },
-            {
-                id: 2,
-                link: "Contact Us",
-                path: "/contact"
-            },
-            {
-                id: 3,
-                link: "Service Centres",
-                path: "/service-centers"
-            },
-            {
-                id: 4,
-                link: "Careers",
-                path: "/careers"
-            },
-            {
-                id: 5,
-                link: "Affiliates",
-                path: "/affiliates"
-            },
-        ]
-    }
+  {
+    id: 1,
+    title: "Help",
+    menu: [
+      { id: 1, link: "FAQs", path: "/faqs" },
+      { id: 2, link: "Track Order", path: "/track-order" },
+      { id: 3, link: "Cancel Order", path: "/cancel-order" },
+      { id: 4, link: "Return Order", path: "/return-policy" },
+      { id: 5, link: "Warranty Info", path: "/warranty" },
+    ]
+  },
+  {
+    id: 2,
+    title: "Policies",
+    menu: [
+      { id: 1, link: "Return Policy", path: "/return-policy" },
+      { id: 2, link: "Security", path: "/security" },
+      { id: 3, link: "Sitemap", path: "/sitemap" },
+      { id: 4, link: "Privacy Policy", path: "/privacy" },
+      { id: 5, link: "Terms & Conditions", path: "/terms" },
+    ]
+  },
+  {
+    id: 3,
+    title: "Company",
+    menu: [
+      { id: 1, link: "About Us", path: "/about" },
+      { id: 2, link: "Contact Us", path: "/contact" },
+      { id: 3, link: "Service Centres", path: "/service-centers" },
+      { id: 4, link: "Careers", path: "/careers" },
+      { id: 5, link: "Affiliates", path: "/affiliates" },
+    ]
+  }
 ];
 
 export const footSocial = [
-    {
-        id: 1,
-        icon: <FaFacebookF />,
-        path: "https://www.facebook.com/share/16mFGNDoB2/?mibextid=wwXIfr",
-    },
-    {
-        id: 2,
-        icon: <FaInstagram />,
-        path: "https://www.instagram.com/platinumperfumes.lb?igsh=MWpvNGhqODE3Z2hwMg==",
-    },
-    {
-        id: 3,
-        icon: <FaTiktok />,
-        path: "https://www.tiktok.com/@platinumperfumes.lb1?_t=ZS-8yl8PGeWCCm&_r=1",
-    },
-    {
-        id: 4,
-        icon: <FaYoutube />,
-        path: "https://youtube.com/@platinumperfumes?si=aWdt_ZGY54Yxb2ye",
-    },
-    {
-        id: 5,
-        icon: <FaSnapchat />,
-        path: "https://t.snapchat.com/WVMktY8Q",
-    }
+  {
+    id: 1,
+    icon: <FaFacebookF />,
+    path: "https://www.facebook.com/share/16mFGNDoB2/?mibextid=wwXIfr",
+  },
+  {
+    id: 2,
+    icon: <FaInstagram />,
+    path: "https://www.instagram.com/platinumperfumes.lb?igsh=MWpvNGhqODE3Z2hwMg==",
+  },
+  {
+    id: 3,
+    icon: <FaTiktok />,
+    path: "https://www.tiktok.com/@platinumperfumes.lb1?_t=ZS-8yl8PGeWCCm&_r=1",
+  },
+  {
+    id: 4,
+    icon: <FaYoutube />,
+    path: "https://youtube.com/@platinumperfumes?si=aWdt_ZGY54Yxb2ye",
+  },
+  {
+    id: 5,
+    icon: <FaSnapchat />,
+    path: "https://t.snapchat.com/WVMktY8Q",
+  },
+  {
+    id: 6,
+    icon: <FaMapMarkerAlt />,
+    path: "https://maps.app.goo.gl/DwPxnwmsZxRztkDM8",
+  }
 ];
