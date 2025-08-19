2b7aa8941cfd582bcdbd12ddb73b0b1151069e17
GK <gabykassab0@gmail.com>
2025-08-19 15:45:50 +0300

    Updated Black XS image

--- src/routes/RouterRoutes.js
+++ src/routes/RouterRoutes.js
@@ -220,6 +220,73 @@
 
 
 
+// import React from 'react';
+// import { Routes, Route } from 'react-router-dom';
+// import useScrollRestore from '../hooks/useScrollRestore';
+
+// // Pages
+// import Home from '../pages/Home';
+// import AllProducts from '../pages/AllProducts';
+// import ProductsPage from '../page/ProductsPage';
+// import ProductDetails from '../componen/ProductDetails';
+// import ErrorPage from '../pages/ErrorPage';
+// import SearchResults from '../page/SearchResults';
+
+// // Components
+// import CartPage from '../componen/CartPage';
+// import CheckoutPage from '../componen/CheckoutPage';
+
+// const RouterRoutes = () => {
+//   useScrollRestore();
+
+//   return (
+//     <Routes>
+//       <Route path="/" element={<Home />} />
+//       <Route path="/cart" element={<CartPage />} />
+//       <Route path="/checkout" element={<CheckoutPage />} />
+//       <Route path="/all-products" element={<AllProducts />} />
+
+//       {/* Product detail page */}
+//       <Route path="/product-details/:productId" element={<ProductDetails />} />
+
+//       {/* Product listing */}
+//       <Route path="/perfumes" element={<ProductsPage />} />
+//       <Route path="/perfumes/men" element={<ProductsPage defaultGenre="men" />} />
+//       <Route path="/perfumes/women" element={<ProductsPage defaultGenre="women" />} />
+//       <Route path="/lattafa-rasasi" element={<ProductsPage />} />
+//       <Route path="/original" element={<ProductsPage />} />
+//       <Route path="/makeup" element={<ProductsPage />} />
+//       <Route path="/watches" element={<ProductsPage />} />
+//       <Route path="/musk" element={<ProductsPage />} />
+//       <Route path="/refresheners" element={<ProductsPage />} />
+
+//       {/* 🔍 Search results page */}
+//       <Route path="/search" element={<SearchResults />} />
+
+//       {/* 404 Fallback */}
+//       <Route path="*" element={<ErrorPage />} />
+//     </Routes>
+//   );
+// };
+
+// export default RouterRoutes;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import useScrollRestore from '../hooks/useScrollRestore';
@@ -259,6 +326,8 @@ const RouterRoutes = () => {
       <Route path="/watches" element={<ProductsPage />} />
       <Route path="/musk" element={<ProductsPage />} />
       <Route path="/refresheners" element={<ProductsPage />} />
+      <Route path="/body-mist" element={<ProductsPage />} />   {/* 🆕 Body Mist & Deodorant */}
+      <Route path="/body-cream" element={<ProductsPage />} />  {/* 🆕 Body Cream */}
 
       {/* 🔍 Search results page */}
       <Route path="/search" element={<SearchResults />} />
